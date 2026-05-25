369258f6f973b03f9e6eac4b56eb9ebf2da7468d
Quang Long <dquanglong682005@gmail.com>
2026-05-25 14:17:31 +0700

    Initial commit: Spring Boot backend setup

---
+++ src/main/java/com/longdq/adaptengbackend/AdaptEngBackendApplication.java
@@ -0,0 +1,13 @@
+package com.longdq.adaptengbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptEngBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptEngBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/longdq/adaptengbackend/AdaptEngBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longdq.adaptengbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptEngBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
